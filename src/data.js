8e8e804647fd61715624da69ecb4470ef46ba0a2
BukharinovMaksym <lunar.blossom.contact@gmail.com>
2023-05-30 22:34:53 +0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "MYGAME1",
+    subtitle: "C++, Unreal Engine",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "#projects",
+  },
+  {
+    title: "MYGAME2",
+    subtitle: "C#, Unity Engine",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "#projects",
+  },
+  {
+    title: "MYGAME3",
+    subtitle: "C#, Unity Engine",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "#projects",
+  },
+  {
+    title: "MYGAME4",
+    subtitle: "C++, Unreal Engine",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "#projects",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Hey, there is a great opportunity for you to become one of my first clients and leave a good testimonial =). As I was creating projects only for myself and my practical learning currently I don't have any.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Probably you",
+    company: "You'r company",
+  },
+  {
+    quote:
+      "Hey, there is a great opportunity for you to become one of my first clients and leave a good testimonial =). As I was creating projects only for myself and my practical learning currently I don't have any.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Probably you",
+    company: "You'r company",
+  },
+];
+
+export const skills = [
+  "C#/C++",
+  "Adobe Photoshop",
+  "Unreal Engine",
+  "Adobe Animate",
+  "Unity Engine",
+  "Blender",
+];
